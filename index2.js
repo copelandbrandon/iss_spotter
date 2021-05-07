1841ca72206ce50f37b0acd3006195d8e2606a91
Brandon Copeland <copelandbrandon2@gmail.com>
2021-05-07 01:29:41 +0000

    refactored code to use promises and avoid callback hell

---
+++ index2.js
@@ -0,0 +1,18 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`)
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error)=>{
+    console.log("Oh no something went wrong:", error.message);
+  })
